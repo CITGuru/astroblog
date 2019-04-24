47afc67b0c79efe2aa2633ba37af1353442a94fa
Oyetoke Toby <oyetoketoby80@gmail.com>
2019-04-24 07:31:28 +0100

    Done with Banner, Bread Banner, workinng on Blog Lists

--- src/Components/BreadBanner.js
+++ src/Components/BreadBanner.js
@@ -6,25 +6,25 @@ export default class BreadBanner extends Component {
       
       <div className="bread-wrapper">
         <div className="container bread-inner">
-            <div className="">
-                <ul className="nav">
-                    <li className="nav-item">
-                    <a className="nav-link active" href="#">All</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Business</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Productivity</a>
-                    </li>
-                </ul>
+            <div className="row">
+                <div className="col-8 mt-auto mb-auto">
+                    <ul className="nav">
+                        <li className="nav-item">
+                        <a className="nav-link active" href="#">All</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Business</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Productivity</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-4 ml-auto">
+                    <input type="text" className="form-control" id="searchbox" placeholder="Search" />
+                </div>
             </div>
-            <div className="ml-auto">
-                <input type="text" className="form-control" id="searchbox" placeholder="First name" value="Mark" required />
-             </div>
         </div>
-        
-        
       </div>
         
     )
